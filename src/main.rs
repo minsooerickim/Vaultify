f4bc65866f88c2f3a3ba4a39dc987e1554bf52b3
Minsoo Kim <minsooerickim@gmail.com>
2023-05-27 21:01:36 -0700

    :building_construction: setup diesel + 1 endpoint
    configured diesel and setup a simple auth api that retrives users from db

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
+#[macro_use] extern crate diesel;
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+use diesel::prelude::*;
+use std::fmt::Write;
+
+use rocket_sync_db_pools::database;
+
+mod schema; // module auto-generated schema from diesel migration
+mod models;
+
+use models::*;
+#[database("sqlite_data_db")]
+struct DataDatabase(diesel::SqliteConnection);
+
+#[get("/auth")]
+async fn authenticate(mut conn: DataDatabase) -> String {
+    conn.run(|c| load_from_db(c)).await
+}
+
+fn load_from_db(conn: &mut diesel::SqliteConnection) -> String {
+    use crate::schema::users::dsl::*;
+
+    let user_list = users.load::<User>(conn).expect("Error reading database.");
+
+    let mut s = String::new();
+
+    for user in user_list {
+        writeln!(s, "{}\n", user.email).expect("Failed to write to string");
+    }
+
+    s
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build()
+        .attach(DataDatabase::fairing())
+        .mount("/api", routes![authenticate])
 }
